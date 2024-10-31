3dcaa32336b34a7b79de26fed3c2a9804fba631e
Majorsaturn <nicoanovak@gmail.com>
2024-10-31 14:39:26 -0400

    Attempted test abstraction

--- src/App.ts
+++ src/App.ts
@@ -1,22 +1,6 @@
 //App.ts
 import {app, BrowserWindow} from 'electron';
-import * as path from 'path';
-
-const DEFAULT_WIDTH: number = 1200;
-const DEFAULT_HEIGHT: number = 760;
-const indexHTML = path.join(__dirname, '../templates/index.html')
-
-function createWindow() {
-    const mainWindow = new BrowserWindow({
-        width: DEFAULT_WIDTH,
-        height: DEFAULT_HEIGHT,
-        webPreferences: {
-            nodeIntegration: true,
-        }
-    });
-
-    mainWindow.loadFile(indexHTML);
-}
+import {createWindow} from './views/HomePageView'
 
 app.on('ready', createWindow);
 
--- src/views/HomePageView.ts
+++ src/views/HomePageView.ts
@@ -1,1 +1,23 @@
 //HomePageView.ts
+import {BrowserWindow} from 'electron';
+import * as path from 'path';
+
+const DEFAULT_WIDTH: number = 1200;
+const DEFAULT_HEIGHT: number = 760;
+const indexHTML = path.join(__dirname, '../../templates/index.html')
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: DEFAULT_WIDTH,
+        height: DEFAULT_HEIGHT,
+        webPreferences: {
+            nodeIntegration: true,
+        }
+    });
+
+    mainWindow.loadFile(indexHTML);
+}
+
+export{
+    createWindow
+}
