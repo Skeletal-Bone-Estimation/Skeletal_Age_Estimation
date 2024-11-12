adf86600b3165dedcc7cd747e0365f2f9d030f71
matt-hardenburg <mzh5947@psu.edu>
2024-11-11 21:44:41 -0500

    made pagecontroller singleton, updated home screen button functionality

--- src/renderer.ts
+++ src/renderer.ts
@@ -3,6 +3,6 @@ const { PageController } = require('./dist/controllers/PageController');
 console.log('renderer loaded');
 
 window.addEventListener('DOMContentLoaded', async () => {
-    const pageController = new PageController();
+    const pageController = PageController.getInstance();
     await pageController.navigateTo('home');
 });
