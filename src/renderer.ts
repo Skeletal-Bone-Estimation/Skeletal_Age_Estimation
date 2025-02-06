e80516587523ed2885183e9e667a0b907a42fc69
Szar28 <100392783+Szar28@users.noreply.github.com>
2025-02-05 23:44:44 -0500

    removed auto chart import that was causing error

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,7 +1,6 @@
 // Edited by: Nicholas Novak, Matthew Szarmach. Matthew Hardenburg, Cassidy Marquis
 
 const { PageController } = require('./dist/controllers/PageController');
-const Chart = require('chart.js/auto');
 
 //console.log('renderer loaded');
 
