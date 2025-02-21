2a1fde84f125871b1239c3bed7672981c5c7af4a
cmar-web <cmariemarquis@icloud.com>
2025-02-20 20:32:29 -0500

    autosaveobserver passes 2/2

--- tests/unit/utils/observer/AutosaveObserver.test.ts
+++ tests/unit/utils/observer/AutosaveObserver.test.ts
@@ -1,45 +1,48 @@
-//AutosaveObserver.test.ts
-import { AutosaveObserver } from '../../../../src/utils/observer/AutosaveObserver';
+// AutosaveObserver.test.ts
+import { AutosaveObserver } from '../../../../src/utils/observer/AutosaveObserver'
 import { DataController } from '../../../../src/controllers/DataController';
 import { XML_Controller } from '../../../../src/controllers/XML_Controller';
-import { AbstractCaseModel } from '../../../../src/models/AbstractCaseModel';
+import { Observers } from '../../../../src/utils/enums';
 import { CaseModel } from '../../../../src/models/CaseModel';
 
 jest.mock('../../../../src/controllers/DataController');
 jest.mock('../../../../src/controllers/XML_Controller');
 
 describe('AutosaveObserver', () => {
-    let autosaveObserver: AutosaveObserver;
+  let autosaveObserver: AutosaveObserver;
+  let mockCase: CaseModel;
+  let mockDataController: any;
+  let mockXMLController: any;
 
-    beforeEach(() => {
-        autosaveObserver = new AutosaveObserver();
+  beforeEach(() => {
+    autosaveObserver = new AutosaveObserver();
 
-        // Mock the CaseModel
-        const mockOpenCase: AbstractCaseModel = {
-            caseID: 'testCase123',
-        } as unknown as CaseModel;
+    // Create a dummy case with a known caseID.
+    mockCase = { caseID: 'dummy123' } as CaseModel;
 
-        // Mock DataController.getInstance().openCase
-        (DataController.getInstance as jest.Mock).mockReturnValue({
-            openCase: mockOpenCase,
-        });
+    // Mock DataController.getInstance() to return an object with openCase equal to our dummy case.
+    mockDataController = { openCase: mockCase };
+    (DataController.getInstance as jest.Mock).mockReturnValue(mockDataController);
 
-        // Mock XML_Controller.getInstance().saveAsFile
-        (XML_Controller.getInstance as jest.Mock).mockReturnValue({
-            saveAsFile: jest.fn(),
-        });
-    });
+    // Mock XML_Controller.getInstance() to return an object with a jest.fn() for saveAsFile.
+    mockXMLController = { saveAsFile: jest.fn() };
+    (XML_Controller.getInstance as jest.Mock).mockReturnValue(mockXMLController);
+  });
 
-    it('should call saveAsFile with the correct parameters when autosave is triggered', () => {
-        autosaveObserver.update();
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
-        const mockOpenCase = (DataController.getInstance() as any).openCase;
-        const saveAsFileMock = XML_Controller.getInstance().saveAsFile;
+  it('should not autosave if update is called with a non-autosave observer', () => {
+    autosaveObserver.update(Observers.setSelectedReport, { some: 'data' });
+    expect(mockXMLController.saveAsFile).not.toHaveBeenCalled();
+  });
 
-        expect(saveAsFileMock).toHaveBeenCalledTimes(1);
-        expect(saveAsFileMock).toHaveBeenCalledWith(
-            mockOpenCase,
-            'save_data/testCase123.xml',
-        );
-    });
+  it('should autosave when update is called with Observers.autosave', () => {
+    autosaveObserver.update(Observers.autosave);
+    expect(mockXMLController.saveAsFile).toHaveBeenCalledWith(
+      mockCase,
+      'save_data/dummy123.xml'
+    );
+  });
 });
