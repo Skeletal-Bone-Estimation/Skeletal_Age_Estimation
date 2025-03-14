9e45afffa9b71b3784888209aeccd801e73aa87a
Szar28 <100392783+Szar28@users.noreply.github.com>
2025-03-14 15:29:40 -0400

    graph compare allows you to change the right side

--- src/views/ComparePageView.ts
+++ src/views/ComparePageView.ts
@@ -8,11 +8,19 @@ import { AbstractReportModel } from '../models/AbstractReportModel';
 import { Side, Pages } from '../utils/enums';
 import { updateRangeBar } from '../utils/charts/ageRangeChart';
 import { NullReportModel } from '../models/NullReportModel';
+import { NullCaseModel } from '../models/NullCaseModel';
+import { AbstractCaseModel } from '../models/AbstractCaseModel';
 
 //TODO: this is supposed to extend ReportPageView to take advantage of exisitng methods
 export class ComparePageView extends AbstractView {
+    private storedReport: string | NullReportModel;
+    private storedCase: CaseModel;
+    private isInitialReportLoaded: boolean = false;
+
     constructor(document: Document) {
         super(document);
+        this.storedReport = new NullReportModel();
+        this.storedCase = DataController.getInstance().openCase as CaseModel;
     }
 
     /**
@@ -24,20 +32,59 @@ export class ComparePageView extends AbstractView {
         this.initEventListeners();
         this.setSidebarListeners();
 
-        const report = DataController.getInstance().openReport;
-        const _case: CaseModel = DataController.getInstance()
+        const reportCompare = DataController.getInstance().openReport;
+        const _caseCompare: CaseModel = DataController.getInstance()
             .openCase as CaseModel;
 
-        // call load report method with the most recent report
-        if (!(report instanceof NullReportModel)) {
-            this.loadReport(
-                _case.generatedReports[
-                    DataController.getInstance().findReportIndex(report)
-                ] as ReportModel,
-            );
-            //console.log('Report data loaded');
+        if (this.isInitialReportLoaded == false) {
+            const report = DataController.getInstance().openReport;
+            const _case: CaseModel = DataController.getInstance()
+                .openCase as CaseModel;
+
+            // call load report method with the most recent report
+            if (!(report instanceof NullReportModel)) {
+                this.storedReport = report;
+                this.storedCase = _case;
+
+                this.loadReport(
+                    _case.generatedReports[
+                        DataController.getInstance().findReportIndex(report)
+                    ] as ReportModel,
+                );
+                //console.log('Report data loaded');
+            } else {
+                console.error('No report found.');
+            }
+
+            this.isInitialReportLoaded = true;
         } else {
-            console.error('No report found.');
+            // call load report method with the most recent report
+            if (!(reportCompare instanceof NullReportModel)) {
+                this.loadReportCompare(
+                    _caseCompare.generatedReports[
+                        DataController.getInstance().findReportIndex(
+                            reportCompare,
+                        )
+                    ] as ReportModel,
+                );
+                //console.log('Report data loaded');
+            } else {
+                console.error('No report found.');
+            }
+
+            // call load report method with the most recent report
+            if (!(this.storedReport instanceof NullReportModel)) {
+                this.loadReport(
+                    this.storedCase.generatedReports[
+                        DataController.getInstance().findReportIndex(
+                            this.storedReport,
+                        )
+                    ] as ReportModel,
+                );
+                //console.log('Report data loaded');
+            } else {
+                console.error('No report found.');
+            }
         }
     }
 
@@ -54,11 +101,10 @@ export class ComparePageView extends AbstractView {
 
         document
             .getElementById('backBtn')!
-            .addEventListener(
-                'click',
-                async () =>
-                    await PageController.getInstance().navigateTo(Pages.Report),
-            );
+            .addEventListener('click', async () => {
+                this.isInitialReportLoaded = false;
+                await PageController.getInstance().navigateTo(Pages.Report);
+            });
     }
 
     /**
@@ -126,6 +172,73 @@ export class ComparePageView extends AbstractView {
 
         // Display the third molar data
         const molarData = document.getElementById('molarDataLeft');
+        if (molarData) {
+            molarData.innerHTML = `
+                <strong>Third Molar:</strong>
+                <p>Top Left: ${this.formatThirdMolar(report.getThirdMolar(Side.TL))}</p>
+                <p>Top Right: ${this.formatThirdMolar(report.getThirdMolar(Side.TR))}</p>
+                <p>Bottom Left: ${this.formatThirdMolar(report.getThirdMolar(Side.BL))}</p>
+                <p>Bottom Right: ${this.formatThirdMolar(report.getThirdMolar(Side.BR))}</p>
+            `;
+        } else {
+            console.error('Element molarData not found!');
+        }
+    }
+
+    /**
+     * Load the report data into the compare page.
+     * @param report The ReportModel to load.
+     */
+    public loadReportCompare(report: ReportModel): void {
+        // Summarized range placeholder
+        const summarizedRange = document.getElementById('summarizedRangeRight');
+        if (summarizedRange) {
+            summarizedRange.textContent = `Summarized Range: ${this.calculateSummarizedRangeCompare(report)}`;
+        } else {
+            console.error('SummarizedRange not found');
+        }
+
+        // Display estimated pubic symphysis range
+        this.displayDataSection(
+            'pubicDataRight',
+            'Pubic Symphysis',
+            report.getPubicSymphysis(Side.L),
+            report.getPubicSymphysis(Side.R),
+            report.getPubicSymphysis(Side.C),
+            report.getPubicSymphysisRange(Side.L),
+            report.getPubicSymphysisRange(Side.R),
+            report.getPubicSymphysisRange(Side.C),
+            'pubicSymphysisBarRight',
+        );
+
+        // Display the auricular surface range
+        this.displayDataSection(
+            'auricularDataRight',
+            'Auricular Surface',
+            report.getAuricularSurface(Side.L),
+            report.getAuricularSurface(Side.R),
+            report.getAuricularSurface(Side.C),
+            report.getAuricularSurfaceRange(Side.L),
+            report.getAuricularSurfaceRange(Side.R),
+            report.getAuricularSurfaceRange(Side.C),
+            'auricularSurfaceBarRight',
+        );
+
+        // Display the sternal end range
+        this.displayDataSection(
+            'sternalDataRight',
+            'Sternal End',
+            report.getSternalEnd(Side.L),
+            report.getSternalEnd(Side.R),
+            report.getSternalEnd(Side.C),
+            report.getSternalEndRange(Side.L),
+            report.getSternalEndRange(Side.R),
+            report.getSternalEndRange(Side.C),
+            'sternalEndBarRight',
+        );
+
+        // Display the third molar data
+        const molarData = document.getElementById('molarDataRight');
         if (molarData) {
             molarData.innerHTML = `
                 <strong>Third Molar:</strong>
@@ -221,6 +334,35 @@ export class ComparePageView extends AbstractView {
 
         // Update the UI range bar (adjust the ID accordingly)
         updateRangeBar(minAgeNum, maxAgeNum, 'ageBarLeft');
+
+        return `${minAge} - ${maxAge}`;
+    }
+
+    private calculateSummarizedRangeCompare(
+        report: AbstractReportModel,
+    ): string {
+        // Get the minimum and maximum age across all ranges
+        if ((report as ReportModel).getThirdMolar(Side.C) === 0) {
+            var minAge = Math.min(
+                report.getPubicSymphysisRange(Side.C).min,
+                report.getAuricularSurfaceRange(Side.C).min,
+                report.getSternalEndRange(Side.C).min,
+            ).toFixed(2);
+        } else {
+            var minAge = '18.00';
+        }
+        const maxAge = Math.max(
+            report.getPubicSymphysisRange(Side.C).max,
+            report.getAuricularSurfaceRange(Side.C).max,
+            report.getSternalEndRange(Side.C).max,
+        ).toFixed(2);
+
+        // Convert min/max to numbers for updateRangeBar
+        const minAgeNum = parseFloat(minAge);
+        const maxAgeNum = parseFloat(maxAge);
+
+        // Update the UI range bar (adjust the ID accordingly)
+        updateRangeBar(minAgeNum, maxAgeNum, 'ageBarRight');
 
         return `${minAge} - ${maxAge}`;
     }
