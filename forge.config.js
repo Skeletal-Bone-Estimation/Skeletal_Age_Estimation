8511bac0160b4ffa4b3e95e9f4ebba3c42a0f325
cmar-web <cmariemarquis@icloud.com>
2025-05-05 21:25:10 -0400

    no ml but icons and separate executables

--- forge.config.js
+++ forge.config.js
@@ -8,27 +8,41 @@ const isMac = process.platform === 'darwin';
 module.exports = {
   buildDirectory: 'out',
   name: 'Skeletage',
-  icon: path.resolve(__dirname, 'static', 'icons', 'icon'),
+  
+  icon: isMac 
+    ? path.resolve(__dirname, 'static', 'images', 'Skeletage_Logo_2025.icns')
+    : path.resolve(__dirname, 'static', 'images', 'Skeletage_Logo_2025.ico'),
   packagerConfig: {
     asar: {
       unpack: '{static/**,server.exe,models/**}',
     },
+    icon: isMac 
+      ? path.resolve(__dirname, 'static', 'images', 'Skeletage_Logo_2025.icns')
+      : path.resolve(__dirname, 'static', 'images', 'Skeletage_Logo_2025.ico'),
     extraResource: [
       path.resolve(__dirname, 'src', 'ml', 'models'),
-      path.resolve(__dirname, 'src', 'ml', 'dist', isMac ? 'server' : 'server.exe'), 
+      path.resolve(__dirname, 'src', 'ml', 'dist', isMac ? 'server' : 'server.exe'),
       path.resolve(__dirname, 'static')
     ],
   },
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: { name: 'Skeletage' },
+      config: {
+        name: 'Skeletage',
+        
+        setupIcon: path.resolve(__dirname, 'static', 'images', 'Skeletage_Logo_2025.ico'),
+      
+        iconUrl: path.resolve(__dirname, 'static', 'images', 'Skeletage_Logo_2025.ico'),
+      },
     },
     {
       name: '@electron-forge/maker-dmg',
       config: {
         name: 'Skeletage',
         format: 'ULFO',
+       
+        icon: path.resolve(__dirname, 'static', 'images', 'Skeletage_Logo_2025.icns'),
       }
     },
     {
